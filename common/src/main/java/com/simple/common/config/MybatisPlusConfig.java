c7e436ffb355194bb96fd03f44c7a5898e7d2418
swen <1063642821@qq.com>
2020-01-17 16:10:10 +0800

    初始化版本

--- common/src/main/java/com/simple/common/config/MybatisPlusConfig.java
+++ common/src/main/java/com/simple/common/config/MybatisPlusConfig.java
@@ -1,14 +1,16 @@
 package com.simple.common.config;
 
 
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.apache.ibatis.type.JdbcType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
- * MybatisPlus 分页设置
+ * MybatisPlus 设置
  * @author swen
  */
 @EnableTransactionManagement
@@ -21,5 +23,10 @@ public class MybatisPlusConfig {
         // 开启 count 的 join 优化,只针对部分 left join
         paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
         return paginationInterceptor;
+    }
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer(){
+        return configuration -> configuration.setJdbcTypeForNull(JdbcType.NULL);
     }
 }
