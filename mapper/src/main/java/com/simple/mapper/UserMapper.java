66542c7f15480a2c7072698ebe90ffb2e207165a
swen <1063642821@qq.com>
2020-01-17 14:47:20 +0800

    初始化版本

---
+++ mapper/src/main/java/com/simple/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.simple.mapper;
+
+import com.simple.model.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户信息 Mapper 接口
+ * </p>
+ *
+ * @author swen
+ * @since 2020-01-17
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ service/src/main/java/com/simple/service/IUserService.java
@@ -0,0 +1,19 @@
+package com.simple.service;
+
+import com.simple.model.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.simple.model.PageRequest;
+import com.simple.model.PageResponse;
+
+/**
+ * <p>
+ * 用户信息 服务类
+ * </p>
+ *
+ * @author swen
+ * @since 2020-01-17
+ */
+public interface IUserService extends IService<User> {
+
+     PageResponse<User> userList(User user, PageRequest pageRequest);
+}
